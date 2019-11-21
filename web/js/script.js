727ed8dd1a7f71de4ba0a9d25df0b067dc62fdb9
Paulo <pauloh16.pdr@gmail.com>
2019-11-21 11:52:31 -0300

    Criar página contato

--- web/js/script.js
+++ web/js/script.js
@@ -1,4 +1,5 @@
 const iconesRedes = document.querySelectorAll('.footer_redes img');
+const iconesRedesContato = document.querySelectorAll('.contato_dados li img');
 
 function mudarIcone(item){
 	if (item == 1){
@@ -13,5 +14,17 @@ function mudarIcone(item){
 		iconesRedes[1].src = 'img/sociais/instagram.svg';
 	}else if(item == 6){
 		iconesRedes[2].src = 'img/sociais/twitter.svg'
-	}
+	}else if(item == 7){
+		iconesRedesContato[0].src = 'img/sociais/facebook-preto.svg'
+	}else if(item == 8){
+		iconesRedesContato[1].src = 'img/sociais/instagram-preto.svg'
+	}else if(item == 9){
+		iconesRedesContato[2].src = 'img/sociais/twitter-preto.svg'
+	}else if(item == 10){
+		iconesRedesContato[0].src = 'img/sociais/facebook.svg';
+	}else if(item == 11){
+		iconesRedesContato[1].src = 'img/sociais/instagram.svg';
+	}else if(item == 12){
+		iconesRedesContato[2].src = 'img/sociais/twitter.svg'
+	};
 };
