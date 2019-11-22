8b8caba762120e8eefa1ca436689519e30dc80ee
Paulo <pauloh16.pdr@gmail.com>
2019-11-22 03:27:57 -0300

    Adicionar responsividade para Home e Footer

--- web/js/script.js
+++ web/js/script.js
@@ -1,30 +1,52 @@
 const iconesRedes = document.querySelectorAll('.footer_redes img');
 const iconesRedesContato = document.querySelectorAll('.contato_dados li img');
+const logo = document.getElementById('logo');
 
 function mudarIcone(item){
-	if (item == 1){
-		iconesRedes[0].src = 'img/sociais/facebook-branco.svg';
-	}else 	if (item == 2){
-		iconesRedes[1].src = 'img/sociais/instagram-branco.svg';
-	}else 	if (item == 3){
-		iconesRedes[2].src = 'img/sociais/twitter-branco.svg';
-	}else if(item == 4){
-		iconesRedes[0].src = 'img/sociais/facebook.svg';
-	}else if(item == 5){
-		iconesRedes[1].src = 'img/sociais/instagram.svg';
-	}else if(item == 6){
-		iconesRedes[2].src = 'img/sociais/twitter.svg'
-	}else if(item == 7){
-		iconesRedesContato[0].src = 'img/sociais/facebook-preto.svg'
-	}else if(item == 8){
-		iconesRedesContato[1].src = 'img/sociais/instagram-preto.svg'
-	}else if(item == 9){
-		iconesRedesContato[2].src = 'img/sociais/twitter-preto.svg'
-	}else if(item == 10){
-		iconesRedesContato[0].src = 'img/sociais/facebook.svg';
-	}else if(item == 11){
-		iconesRedesContato[1].src = 'img/sociais/instagram.svg';
-	}else if(item == 12){
-		iconesRedesContato[2].src = 'img/sociais/twitter.svg'
+	
+	switch(item){
+		case 1:
+			iconesRedes[0].src = 'img/sociais/facebook-branco.svg';
+			break;
+		case 2:
+			iconesRedes[1].src = 'img/sociais/instagram-branco.svg';
+			break;
+		case 3:
+			iconesRedes[2].src = 'img/sociais/twitter-branco.svg';
+			break;
+		case 4:
+			iconesRedes[0].src = 'img/sociais/facebook.svg';
+			break;
+		case 5:
+			iconesRedes[1].src = 'img/sociais/instagram.svg';
+			break;
+		case 6:
+			iconesRedes[2].src = 'img/sociais/twitter.svg';
+			break;
+		case 7:
+			iconesRedesContato[0].src = 'img/sociais/facebook-preto.svg';
+			break;
+		case 8:
+			iconesRedesContato[1].src = 'img/sociais/instagram-preto.svg';
+			break;
+		case 9:
+			iconesRedesContato[2].src = 'img/sociais/twitter-preto.svg';
+			break;
+		case 10:
+			iconesRedesContato[0].src = 'img/sociais/facebook.svg';
+			break;
+		case 11:
+			iconesRedesContato[1].src = 'img/sociais/instagram.svg';
+			break;
+		case 12:
+			iconesRedesContato[2].src = 'img/sociais/twitter.svg';
+			break;
+		case 13:
+			logo.src = 'img/bikcraft-branco.svg';
+			break;
+		case 14:
+			logo.src = 'img/bikcraft.svg';
+		default:
+			break;
 	};
 };
